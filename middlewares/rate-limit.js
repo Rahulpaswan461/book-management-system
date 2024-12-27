fca4741e83814c28406fd86b87109cf6efd42638
Rahul Paswan <rahulpaswandoon@gmail.com>
2024-12-27 20:28:42 +0530

    save changes

--- middlewares/rate-limit.js
+++ middlewares/rate-limit.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit")
 
 const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 15 minutes
+    windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per windowMs
     message: { error: 'Too many requests, please try again later.' },
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
